d115cc63c6c75db3d704f7671e8a55e6f27cbf67
jdelaney <jordan.delaney@me.com>
2020-11-10 21:59:52 -0500

    Secret key fix

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '6j_+94%2sy5_w9324ufmk2!irdd4a@3&-l)lvmb$_p-@9cwew1'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '6j_+94%2sy5_w9324ufmk2!irdd4a@3&-l)lvmb$_p-@9cwew1')
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
